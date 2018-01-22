86941e2881de13dbd45fa792c4898eb7b8d9307a
Simon Muller <samullers@gmail.com>
2018-01-22 20:12:59 +0200

    Build table dependency graph and print info
    We use the networkx library to build up a graph of table dependencies based on their foreign keys. This allows us to find circular dependencies and a an insertion order that might be usable by an import script if circular dependencies do not cause issues.

--- pg_connect.py
+++ pg_connect.py
@@ -1,6 +1,7 @@
 import config
 import db_meta
 import psycopg2
+import networkx as nx
 
 
 def format_tuple(str_args):
@@ -17,7 +18,42 @@ def print_rows(cursor, sql):
     return count
 
 
+def print_cycle_info_and_break_cycles(table_graph):
+    """
+    Changes given graph by breaking cycles
+    """
+    simple_cycles = list(nx.simple_cycles(table_graph))
+    if len(simple_cycles) > 0:
+        print("\n%s self-references and simple cycles found:" % (len(simple_cycles),))
+        print(simple_cycles)
+
+    # Break simple cycles and self-references to help find bigger cycles
+    # copy_of_graph = table_graph.copy()
+    for cycle in nx.simple_cycles(table_graph):
+        table_graph.remove_edge(cycle[0], cycle[-1])
+    try:
+        cycle = nx.find_cycle(table_graph)
+        print("\nAnother cycle was detected:")
+        print(cycle)
+    except:
+        pass
+
+
+def print_partition_info(table_graph):
+    sub_graphs = [graph for graph in nx.weakly_connected_component_subgraphs(table_graph)]
+    if len(sub_graphs) > 1:
+        print("\nReferences can be partitioned into %s sub-graphs:" % (len(sub_graphs),))
+        for graph in sub_graphs:
+            print(graph.nodes())
+
+
+def print_insertion_order(table_graph):
+    print("\nInsertion order:")
+    print(nx.topological_sort(table_graph, reverse=True))
+
+
 def main():
+    VERBOSE = False
     # Connect to an existing database
     conn = psycopg2.connect(
         host=config.DB_HOST,
@@ -27,26 +63,44 @@ def main():
     # Open a cursor to perform database operations
     cur = conn.cursor()
     # Query the database and obtain data as Python objects
+
+    # Process database structure
     schema = "public"
     tables = db_meta.get_table_names(cur, schema)
 
-    print("Found %s tables in schema '%s'" % (len(tables), schema))
     no_pks = []
+    table_graph = nx.DiGraph()
     for table in tables:
-        # print("\n", table)
-        # print(db_meta.get_columns(cur, table, schema))
-        # print(db_meta.get_foreign_keys(cur, table))
+        if VERBOSE:
+            print("\n", table)
+            print(db_meta.get_columns(cur, table, schema))
+            print(db_meta.get_foreign_keys(cur, table))
+
         pks = db_meta.get_primary_key_column_names(cur, table)
         if len(pks) == 0:
             no_pks.append(table)
-    if len(no_pks) > 0:
-        print("The following tables have no primary key: ", no_pks)
+
+        fks = db_meta.get_foreign_keys(cur, table)
+        table_graph.add_node(table)
+        for fk in fks:
+            table_graph.add_edge(fk.table, fk.other_table)
 
     # Make the changes to the database persistent
     conn.commit()
     # Close communication with the database
     cur.close()
     conn.close()
+
+    # Print warnings and info
+    print("Found %s tables in schema '%s'" % (len(tables), schema))
+    print(tables)
+    if len(no_pks) > 0:
+        print("\n%s tables have no primary key:"% (len(no_pks),))
+        print(no_pks)
+
+    print_partition_info(table_graph)
+    print_cycle_info_and_break_cycles(table_graph)
+    print_insertion_order(table_graph)
 
 
 if __name__ == "__main__":
