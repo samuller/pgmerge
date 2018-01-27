4199b5c7fa2acc8a4db2b0f8c02aab64f09c4e7c
Simon Muller <samullers@gmail.com>
2018-01-27 09:01:35 +0200

    Use context to 'close' database connections
    Closing connection will actually release it back to the SQLAlchemy’s connection pool.

--- pg_merge.py
+++ pg_merge.py
@@ -11,16 +11,16 @@ except ImportError:
 
 
 def export_all(engine, inspector, schema, output_dir, file_format="CSV HEADER"):
-    fake_conn = engine.raw_connection()
-    fake_cur = fake_conn.cursor()
+    with engine.raw_connection() as conn:
+        cursor = conn.cursor()
 
-    tables = sorted(inspector.get_table_names(schema))
-    for table in tables:
-        output_file = open(os.path.join(output_dir, table + '.csv'), 'wb')
-        copy_sql = 'COPY %s TO STDOUT WITH %s' % (table, file_format)
-        fake_cur.copy_expert(copy_sql, output_file)
+        tables = sorted(inspector.get_table_names(schema))
+        for table in tables:
+            output_file = open(os.path.join(output_dir, table + '.csv'), 'wb')
+            copy_sql = 'COPY %s TO STDOUT WITH %s' % (table, file_format)
+            cursor.copy_expert(copy_sql, output_file)
 
-    fake_conn.commit()
+        conn.commit()
 
 
 def get_unique_columns(inspector, table, schema):
@@ -31,52 +31,52 @@ def get_unique_columns(inspector, table, schema):
 
 
 def import_all(engine, inspector, schema, input_dir, file_format="CSV HEADER"):
-    fake_conn = engine.raw_connection()
-    fake_cur = fake_conn.cursor()
-    # assert fake_conn.server_version >= 90500, \
-    #     'Postgresql 9.5 or later required for INSERT ... ON CONFLICT: %s' % (fake_conn.server_version,)
+    with engine.raw_connection() as conn:
+        cursor = conn.cursor()
+        # assert conn.server_version >= 90500, \
+        #     'Postgresql 9.5 or later required for INSERT ... ON CONFLICT: %s' % (conn.server_version,)
 
-    tables = sorted(inspector.get_table_names(schema))
-    for table in tables:
-        id_columns = get_unique_columns(inspector, table, schema)
-        if len(id_columns) == 0:
-            print("Skipping table '%s' as it has no primary key or unique columns!" % (table,))
-            continue
-        all_columns = [col['name'] for col in inspector.get_columns(table, schema)]
+        tables = sorted(inspector.get_table_names(schema))
+        for table in tables:
+            id_columns = get_unique_columns(inspector, table, schema)
+            if len(id_columns) == 0:
+                print("Skipping table '%s' as it has no primary key or unique columns!" % (table,))
+                continue
+            all_columns = [col['name'] for col in inspector.get_columns(table, schema)]
 
-        temp_table_name = "_tmp_%s" % (table,)
-        input_file = open(os.path.join(input_dir, table + '.csv'), 'r')
-        # Create temporary table with same columns and types as target table
-        create_sql = "CREATE TEMP TABLE %s AS SELECT * from %s LIMIT 0;" % (temp_table_name, table)
-        fake_cur.execute(create_sql)
+            temp_table_name = "_tmp_%s" % (table,)
+            input_file = open(os.path.join(input_dir, table + '.csv'), 'r')
+            # Create temporary table with same columns and types as target table
+            create_sql = "CREATE TEMP TABLE %s AS SELECT * from %s LIMIT 0;" % (temp_table_name, table)
+            cursor.execute(create_sql)
 
-        copy_sql = 'COPY %s FROM STDOUT WITH %s' % (temp_table_name, file_format)
-        fake_cur.copy_expert(copy_sql, input_file)
+            copy_sql = 'COPY %s FROM STDOUT WITH %s' % (temp_table_name, file_format)
+            cursor.copy_expert(copy_sql, input_file)
 
-        fake_cur.execute("SELECT count(*) from %s;" % (temp_table_name,))
-        # print("%s: %s" % (table, fake_cur.fetchone()[0]))
+            cursor.execute("SELECT count(*) from %s;" % (temp_table_name,))
+            # print("%s: %s" % (table, cursor.fetchone()[0]))
 
-        # select_columns = ",".join(id_columns)
-        # fake_cur.execute("SELECT %s from %s WHERE all columns equal;" % (select_columns, table, ))
-        # # to_be_skipped = fake_cur.fetchall()
-        #
-        # insert_sql = "INSERT INTO %s SELECT * FROM %s WHERE 1 = 1;" % (table, temp_table_name)
-        # fake_cur.execute(insert_sql)
+            # select_columns = ",".join(id_columns)
+            # cursor.execute("SELECT %s from %s WHERE all columns equal;" % (select_columns, table, ))
+            # # to_be_skipped = cursor.fetchall()
+            #
+            # insert_sql = "INSERT INTO %s SELECT * FROM %s WHERE 1 = 1;" % (table, temp_table_name)
+            # cursor.execute(insert_sql)
 
-        # UPDATE table_b SET column1 = a.column1, column2 = a.column2, column3 = a.column3
-        # FROM table_a WHERE table_a.id = table_b.id AND table_b.id in (1, 2, 3)
-        set_columns = ",".join(["%s = %s.%s" % (col, temp_table_name, col)
-                                for col in all_columns])
-        match_columns = " AND ".join(["%s.%s = %s.%s" % (table, col, temp_table_name, col)
-                                      for col in id_columns])
-        update_sql = "UPDATE %s SET %s FROM %s WHERE %s" % (table, set_columns, temp_table_name, match_columns)
-        fake_cur.execute(update_sql)
-        print("Updated %s rows in '%s'" % (fake_cur.rowcount, table))
+            # UPDATE table_b SET column1 = a.column1, column2 = a.column2, column3 = a.column3
+            # FROM table_a WHERE table_a.id = table_b.id AND table_b.id in (1, 2, 3)
+            set_columns = ",".join(["%s = %s.%s" % (col, temp_table_name, col)
+                                    for col in all_columns])
+            match_columns = " AND ".join(["%s.%s = %s.%s" % (table, col, temp_table_name, col)
+                                          for col in id_columns])
+            update_sql = "UPDATE %s SET %s FROM %s WHERE %s" % (table, set_columns, temp_table_name, match_columns)
+            cursor.execute(update_sql)
+            print("Updated %s rows in '%s'" % (cursor.rowcount, table))
 
-        drop_sql = "DROP TABLE %s" % (temp_table_name,)
-        fake_cur.execute(drop_sql)
+            drop_sql = "DROP TABLE %s" % (temp_table_name,)
+            cursor.execute(drop_sql)
 
-    fake_conn.commit()
+        conn.commit()
 
 
 @click.command(context_settings=dict(max_content_width=120))
