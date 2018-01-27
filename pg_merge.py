71dc2cbc98c039f13e332da40b859bbfe7d5526a
Simon Muller <samullers@gmail.com>
2018-01-27 08:54:26 +0200

    Change unique_columns function to combine all columns

--- pg_merge.py
+++ pg_merge.py
@@ -25,14 +25,9 @@ def export_all(engine, inspector, schema, output_dir, file_format="CSV HEADER"):
 
 def get_unique_columns(inspector, table, schema):
     pks = inspector.get_primary_keys(table, schema)
-    if len(pks) > 0:
-        return pks
-
-    uniques = inspector.get_unique_constraints(table, schema)
-    if len(uniques) == 0:
-        return []
-    else:
-        return uniques[0]['column_names']
+    unique_constraints = inspector.get_unique_constraints(table, schema)
+    unique = [col for constraint in unique_constraints for col in constraint['column_names']]
+    return pks + unique
 
 
 def import_all(engine, inspector, schema, input_dir, file_format="CSV HEADER"):
@@ -43,6 +38,10 @@ def import_all(engine, inspector, schema, input_dir, file_format="CSV HEADER"):
 
     tables = sorted(inspector.get_table_names(schema))
     for table in tables:
+        id_columns = get_unique_columns(inspector, table, schema)
+        if len(id_columns) == 0:
+            print("Skipping table '%s' as it has no primary key or unique columns!" % (table,))
+            continue
         temp_table_name = "_tmp_%s" % (table,)
         input_file = open(os.path.join(input_dir, table + '.csv'), 'r')
         # Create temporary table with same columns and types as target table
@@ -55,10 +54,6 @@ def import_all(engine, inspector, schema, input_dir, file_format="CSV HEADER"):
         fake_cur.execute("SELECT count(*) from %s;" % (temp_table_name,))
         # print("%s: %s" % (table, fake_cur.fetchone()[0]))
 
-        uniques = get_unique_columns(inspector, table, schema)
-        if len(uniques) == 0:
-            print("Table '%s' has no primary key or unique columns to use!" % (table,))
-            continue
 
         drop_sql = "DROP TABLE %s" % (temp_table_name,)
         fake_cur.execute(drop_sql)
