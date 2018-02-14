9aaa206f369dce8448faa857e2ac840a99e87621
Simon Muller <samullers@gmail.com>
2018-02-14 19:23:11 +0200

    Log sql queries on export

--- pgmerge/db_export.py
+++ pgmerge/db_export.py
@@ -8,12 +8,18 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
+def log_sql(sql):
+    log.debug('SQL: {}'.format(sql))
+
+
 def export_all(connection, schema, output_dir, tables=None, file_format="FORMAT CSV, HEADER"):
     cursor = connection.cursor()
 
     for table in tables:
         output_file = open(os.path.join(output_dir, table + '.csv'), 'wb')
         copy_sql = 'COPY %s.%s TO STDOUT WITH (%s)' % (schema, table, file_format)
+        log_sql(copy_sql)
         cursor.copy_expert(copy_sql, output_file)
 
     connection.commit()
