93f4fac0f7382e86eb22974dfbdbc4c8c4266f5f
Simon Muller <samullers@gmail.com>
2018-02-17 12:24:54 +0200

    Add test exporting single table to file

--- pgmerge/tests/test_cli.py
+++ pgmerge/tests/test_cli.py
@@ -1,17 +1,40 @@
-import click
-import unittest
+import os
 from pgmerge import pgmerge
+from .test_db import TestDB
 from click.testing import CliRunner
 
 
-class TestCLI(unittest.TestCase):
+class TestCLI(TestDB):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestCLI, cls).setUpClass()
+        cls.runner = CliRunner()
 
     def test_basics(self):
-        runner = CliRunner()
-        result = runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'NOTICE'])
+        result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'NOTICE'])
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output, "Directory not found: 'NOTICE'\n")
 
-        result = runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'tmp'])
+        result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'tmp'])
         self.assertEquals(result.output, "Exported 0 tables\n")
+
+    def test_export_table(self):
+        self.connection.execute("DROP TABLE IF EXISTS films;")
+        self.connection.execute("""
+        CREATE TABLE films (
+            code        char(5) CONSTRAINT firstkey PRIMARY KEY,
+            title       varchar(40) NOT NULL,
+            did         integer NOT NULL,
+            date_prod   date,
+            kind        varchar(10),
+            len         interval hour to minute
+        );
+        """)
+
+        result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'tmp'])
+        self.assertEquals(result.output, "Exported 1 tables\n")
+        os.remove('tmp/films.csv')
+
+        self.connection.execute("DROP TABLE IF EXISTS films;")
 
---
+++ pgmerge/tests/test_db.py
@@ -0,0 +1,78 @@
+import os
+import unittest
+import sqlalchemy
+import psycopg2.extensions as psyext
+
+
+class TestDB(unittest.TestCase):
+    """
+    Class for setting up a test database and handling connections to it.
+    """
+
+    db_name = ''
+
+    @classmethod
+    def setUpClass(cls):
+        # Create class variables to be re-used between tests
+        cls.create_db_engine = None
+        cls.initial_db = 'template1'
+        cls.engine = None
+        cls.db_name = 'testdb'
+
+        try:
+            TestDB.create_db(cls, cls.db_name)
+        except Exception as err:
+            TestDB.drop_db(cls, cls.db_name)
+            raise err
+
+    @classmethod
+    def tearDownClass(cls):
+        TestDB.drop_db(cls, cls.db_name)
+
+    def create_db(self, db_name):
+        self.url = os.getenv("DB_TEST_URL")
+        if not self.url:
+            self.skipTest("No database URL set")
+        # Open connection to template database (could build url with sqlalchemy.engine.url.URL)
+        self.create_db_engine = sqlalchemy.create_engine(self.url + self.initial_db)
+        with self.create_db_engine.connect() as conn:
+            conn.connection.set_isolation_level(psyext.ISOLATION_LEVEL_AUTOCOMMIT)
+            conn.execute("DROP DATABASE IF EXISTS " + db_name)
+            conn.execute("CREATE DATABASE " + db_name)
+            # conn.connection.set_isolation_level(psyext.ISOLATION_LEVEL_DEFAULT)
+        # self.connection.close()
+        self.engine = sqlalchemy.create_engine(self.url + db_name)
+
+    def drop_db(self, db_name):
+        if self.engine is not None:
+            self.engine.dispose()
+            self.engine = None
+
+        self.create_db_engine.dispose()
+        self.create_db_engine = sqlalchemy.create_engine(self.url + self.initial_db)
+
+        with self.create_db_engine.connect() as conn:
+            conn.connection.set_isolation_level(psyext.ISOLATION_LEVEL_AUTOCOMMIT)
+            # print(find_open_connections(conn))
+            conn.execute("DROP DATABASE IF EXISTS " + db_name)
+            # self.connection.connection.set_isolation_level(psyext.ISOLATION_LEVEL_DEFAULT)
+        # self.connection.close()
+        self.create_db_engine.dispose()
+
+    def setUp(self):
+        # Open connection before each test
+        self.open_db_conn()
+
+    def tearDown(self):
+        # Close connection after each test
+        self.close_db_conn()
+
+    def open_db_conn(self):
+        self.connection = self.engine.connect()
+
+    def close_db_conn(self):
+        self.connection.close()
+        self.connection = None
+
+    def find_open_connections(self, connection):
+        return connection.execute("SELECT * FROM pg_stat_activity").fetchall()
