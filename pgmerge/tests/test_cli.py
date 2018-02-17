7a79bbca709f11d1dcfa587138a1127b4bb73e3a
Simon Muller <samullers@gmail.com>
2018-02-17 16:36:59 +0200

    Add import/export test that uses unicode characters

--- pgmerge/tests/test_cli.py
+++ pgmerge/tests/test_cli.py
@@ -23,13 +23,37 @@ class TestCLI(TestDB):
         self.assertEquals(result.output, "Exported 0 tables\n")
 
     def test_export_table(self):
-                     )
         table_name = 'country'
         table = Table(table_name, self.metadata,
                       Column('code', String(2), primary_key=True),
                       Column('name', String, nullable=False),
+                      extend_existing=True)
         with create_table(self.engine, table):
             result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', self.output_dir])
             self.assertEquals(result.output, "Exported 1 tables\n")
             os.remove('{}/{}.csv'.format(self.output_dir, table_name))
 
+    def test_export_and_import_with_utf8_values(self):
+        table_name = 'country'
+        table = Table(table_name, self.metadata,
+                      Column('code', String(2), primary_key=True),
+                      Column('name', String, nullable=False),
+                      extend_existing=True)
+        with create_table(self.engine, table):
+            stmt = table.insert().values([
+                ('CI','Côte d’Ivoire'),
+                ('RE', 'Réunion'),
+                ('ST', 'São Tomé and Príncipe')
+            ])
+
+            self.connection.execute(stmt)
+            result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', self.output_dir])
+            self.assertEquals(result.output, "Exported 1 tables\n")
+
+            result = self.runner.invoke(pgmerge.upsert, ['--dbname', 'testdb', self.output_dir, table_name])
+            result_lines = result.output.splitlines()
+            self.assertEquals(result_lines[0], """country:""")
+            self.assertEquals(result_lines[1].strip().split(), ["skip:", "3", "insert:", "0", "update:", "0"])
+            self.assertEquals(result_lines[-1], "1 tables imported successfully")
+
+            os.remove('{}/{}.csv'.format(self.output_dir, table_name))
