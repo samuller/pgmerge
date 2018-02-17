5908c435861b71f7918742763a9139b98faacb34
Simon Muller <samullers@gmail.com>
2018-02-17 16:35:32 +0200

    Separate output directory and hard-coded table name

--- pgmerge/tests/test_cli.py
+++ pgmerge/tests/test_cli.py
@@ -10,6 +10,7 @@ class TestCLI(TestDB):
     @classmethod
     def setUpClass(cls):
         super(TestCLI, cls).setUpClass()
+        cls.output_dir = '_tmp_test'
         cls.runner = CliRunner()
         cls.metadata = MetaData()
 
@@ -18,16 +19,17 @@ class TestCLI(TestDB):
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output, "Directory not found: 'NOTICE'\n")
 
-        result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'tmp'])
+        result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', self.output_dir])
         self.assertEquals(result.output, "Exported 0 tables\n")
 
     def test_export_table(self):
-        table_name = 'film'
-        film = Table(table_name, self.metadata,
-                     Column('code', Integer, primary_key=True),
-                     Column('name', String(16), nullable=False)
                      )
-        with create_table(self.engine, film):
-            result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', 'tmp'])
+        table_name = 'country'
+        table = Table(table_name, self.metadata,
+                      Column('code', String(2), primary_key=True),
+                      Column('name', String, nullable=False),
+        with create_table(self.engine, table):
+            result = self.runner.invoke(pgmerge.export, ['--dbname', 'testdb', self.output_dir])
             self.assertEquals(result.output, "Exported 1 tables\n")
-            os.remove('tmp/{}.csv'.format(table_name))
+            os.remove('{}/{}.csv'.format(self.output_dir, table_name))
+
