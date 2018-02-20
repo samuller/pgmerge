d3098c3c4e69dd32df6177258f4909b5af754222
Simon Muller <samullers@gmail.com>
2018-02-20 23:15:39 +0200

    Add schema for tables config file

--- pgmerge/db_config.py
+++ pgmerge/db_config.py
@@ -14,15 +14,14 @@ from appdirs import user_config_dir
 
 _log = logging.getLogger(__name__)
 
-SCHEMA_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'default_config_schema.yml')
+SCHEMA_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tables_config_schema.yml')
 DB_CONFIG_FILE = "db_config.yml"
 PGPASS_FILE = ".pgpass"
 
 
-def load_config_for_db(appname, dbname, priority_config_for_db=None):
+def load_config_for_tables(config_path):
     """
-    Loads any config for the specific database name from the default config file, but
-    then merges those configs with the given configs which take higher priority.
+    Loads a config defining how tables should be imported and exported.
     """
     # Load YAML defining schema for validation of default config
     schema_path = SCHEMA_FILE
@@ -33,28 +32,17 @@ def load_config_for_db(appname, dbname, priority_config_for_db=None):
             rx = Rx.Factory({"register_core_types": True})
             schema = rx.make_schema(schema_config)
     else:
-        _log.debug('Schema file missing (re-install recommended): {}'.format(schema_path))
+        _log.warning('Config schema description is missing (re-install recommended): {}'.format(schema_path))
 
-    # Load default config
-    config_path = os.path.join(user_config_dir(appname, appauthor=False), DB_CONFIG_FILE)
-    ensure_file_exists(config_path)
+    # Load config
     with open(config_path, 'r') as config_file:
         yaml_config = yaml.safe_load(config_file)
 
     # Validate config if it's not empty
     if yaml_config is not None and schema is not None and not schema.check(yaml_config):
-        print("Default config is invalid: '%s'" % (config_path,))
+        _log.warning("Config is invalid: '%s'" % (config_path,))
         return None
-
-    # Assign empty config
-    final_config = {dbname: {'host': None, 'port': None, 'username': None, 'password': None}}
-    # Override empty config with those from default config
-    if yaml_config is not None and dbname in yaml_config:
-        recursive_update_ignore_none(final_config, yaml_config)
-    # Override default config with priority configs
-    if priority_config_for_db is not None:
-        recursive_update_ignore_none(final_config[dbname], priority_config_for_db)
-    return final_config[dbname]
+    return yaml_config
 
 
 def combine_cli_and_db_configs_to_get_url(appname, dbname, host, port, username, password, type="postgresql"):
