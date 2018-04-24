b4c550af72a3af59aec32b4a0cf17c74ffa2d371
Simon Muller <samullers@gmail.com>
2018-04-24 07:45:39 +0200

    Add quotes to print string

--- pgmerge/pgmerge.py
+++ pgmerge/pgmerge.py
@@ -112,7 +112,7 @@ def import_all_new(connection, inspector, schema, import_files, dest_tables, con
 
     if connection.encoding != 'UTF8':
         # raise ExportException('Database connection encoding isn\'t UTF8: {}'.format(connection.encoding))
-        print('WARNING: Setting database connection encoding to UTF8 instead of {}'.format(connection.encoding))
+        print("WARNING: Setting database connection encoding to UTF8 instead of '{}'".format(connection.encoding))
         connection.set_client_encoding('UTF8')
 
     cursor = connection.cursor()
